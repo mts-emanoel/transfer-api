26e55727a2dca6f804f904b07c08478224390064
Matheus Emanoel Rodrigues De Albuquerque <lab.emanoel@gmail.com>
2021-04-11 23:05:14 -0300

    jwt basic structure

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+// API route group
+$router->group(['prefix' => 'auth'], function () use ($router) {
+    $router->post('register', 'AuthController@register');
+    $router->post('login', 'AuthController@login');
+});
